15e645299a8e1f6c524a86a96ef2a8897aff3572
Hitesh <hitesh.ahirwar@techvalens.com>
2025-10-28 12:41:02 +0530

    Adding company config

--- src/test/java/com/henix_erp/configserver/ConfigserverApplicationTests.java
+++ src/test/java/com/henix_erp/configserver/ConfigserverApplicationTests.java
@@ -1,10 +1,10 @@
-package com.henix_erp.config_server;
+package com.henix_erp.configserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ConfigServerApplicationTests {
+class ConfigserverApplicationTests {
 
 	@Test
 	void contextLoads() {
